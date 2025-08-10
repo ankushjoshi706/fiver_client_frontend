8f19ddc4a8009fb858e73369e8170cb5a67fa064
ankushjoshi706 <85740434+ankushjoshi706@users.noreply.github.com>
2025-08-10 23:26:17 +0530

    Update newRequest.js

--- fiverr/src/utils/newRequest.js
+++ fiverr/src/utils/newRequest.js
@@ -4,6 +4,13 @@ const newRequest = axios.create({
     baseURL: import.meta.env.VITE_API_URL, 
     withCredentials: true,
 })
-
+newRequest.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default newRequest;
