a1bd3c75110b902bea8f2edae053dd0892a38c8e
ankushjoshi706 <85740434+ankushjoshi706@users.noreply.github.com>
2025-08-10 23:49:43 +0530

    Update newRequest.js

--- fiverr/src/utils/newRequest.js
+++ fiverr/src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
     baseURL: import.meta.env.VITE_API_URL, 
-    withCredentials: true,
+    
 })
 newRequest.interceptors.request.use(
   (config) => {
